d56a4744378720b74596b6eece1572ffaaaec06a
erikkjell-1 <erkan26@live.se>
2023-09-26 00:05:16 +0200

    commit

--- dictionary-project/src/App.jsx
+++ dictionary-project/src/App.jsx
@@ -6,45 +6,60 @@ function App() {
   const [apiAnswerDef, setApiAnswerDef] = useState('');
   const [apiAnswerPho, setApiAnswerPho] = useState('');
   const [apiAnswerAudio, setApiAnswerAudio] = useState('');
-  const [apiError, setApiError] = useState('');
+  const [apiAnswerMea, setApiAnswerMea] = useState('');
+  const [apiAnswerDef2, setApiAnswerDef2] = useState('');
+  const [apiAnswerMea2, setApiAnswerMea2] = useState('');
   const [apiLoading, setApiLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   async function HandleApi(searchWord) {
-    
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`);
-    const data = await response.json();
-   
-    if (data === null) {
-      setApiError("No Definitions Found");
-      setApiLoading(false);
-      console.log(apiError);
-    } else {
+
+    if (searchWord === '') {
+      setMessage('Please enter a word');
+      setApiLoading(true);
+      return;
+    }
+
+    try {
+      await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`);
+
+      const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`);
+      const data = await response.json();
+
       setApiAnswerDef(data[0].meanings[0].definitions[0].definition);
       setApiAnswerPho(data[0].phonetics[0]);
       setApiAnswerAudio(data[0].phonetics[0].audio);
+      setApiAnswerMea(data[0].meanings[0].partOfSpeech);
+      setApiAnswerMea2(data[0].meanings[1].partOfSpeech);
+      setApiAnswerDef2(data[0].meanings[0].definitions[1].definition);
       setApiLoading(false);
+    } catch (error) {
+      throw new Error(error)
     }
-    
+
   }
   return (
-      <div className='App'>
-        <header>
+    <div className='App'>
+      <header>
         <h1>Dictionary</h1>
-        </header>
+      </header>
 
-        <main>
-          <input type="text" placeholder="Search for a word to get the definition" onChange={e => setSearchWord(e.target.value)} value={searchWord}/>
+      <main>
+        <input type="text" placeholder="Search for a word to get the definition" onChange={e => setSearchWord(e.target.value)} value={searchWord} />
         <button type="submit" onClick={() => HandleApi(searchWord)}>Search</button>
-        </main>
-        <section>
-          <h2>{searchWord}</h2>
-          {apiLoading ? <p>Loading...</p> : <p>{apiError}</p>}
-      
-          {apiLoading ? <p>Loading...</p> : <p>Definition: {apiAnswerDef}</p>}
-          {apiLoading ? <p>Loading...</p> : <p>Phonetic: {apiAnswerPho.text}</p>}
-          <audio controls test-id='audio' src={apiAnswerAudio}></audio>
-        </section>
-        
+      </main>
+      <section>
+        <h2>{searchWord}</h2>
+        <p>{message}</p>
+
+        {apiLoading ? <p>Loading...</p> : <p>Phonetic: {apiAnswerPho.text}</p>}
+        {apiLoading ? <p>Loading...</p> : <p>Part of Speech: {apiAnswerMea}</p>}
+        {apiLoading ? <p>Loading...</p> : <p>Definition: {apiAnswerDef}</p>}
+        {apiLoading ? <p>Loading...</p> : <p>Part of Speech: {apiAnswerMea2}</p>}
+        {apiLoading ? <p>Loading...</p> : <p>Definition: {apiAnswerDef2}</p>}
+        <audio controls test-id='audio' src={apiAnswerAudio}></audio>
+      </section>
+
     </div>
   )
 }

6477fbb8c5e13468086402039d56fc81e898eaf1
erikkjell-1 <erkan26@live.se>
2023-10-17 15:17:33 +0200

    fixed error handling and loading

--- dictionary-project/src/App.jsx
+++ dictionary-project/src/App.jsx
@@ -9,32 +9,32 @@ function App() {
   const [apiAnswerMea, setApiAnswerMea] = useState('');
   const [apiAnswerDef2, setApiAnswerDef2] = useState('');
   const [apiAnswerMea2, setApiAnswerMea2] = useState('');
-  const [apiLoading, setApiLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState('');
 
   async function HandleApi(searchWord) {
-
-    if (searchWord === '') {
-      setMessage('Please enter a word');
-      setApiLoading(true);
+    if (searchWord > 0) {
+      setMessage('Please enter a word to search');
       return;
     }
 
+    setIsLoading(true);
     try {
-      await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`);
-
       const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`);
+      if (!response.ok) {
+        setMessage('No match found for the entered word');
+      }
       const data = await response.json();
-
+      
       setApiAnswerDef(data[0].meanings[0].definitions[0].definition);
       setApiAnswerPho(data[0].phonetics[0]);
       setApiAnswerAudio(data[0].phonetics[0].audio);
       setApiAnswerMea(data[0].meanings[0].partOfSpeech);
       setApiAnswerMea2(data[0].meanings[1].partOfSpeech);
       setApiAnswerDef2(data[0].meanings[0].definitions[1].definition);
-      setApiLoading(false);
-    } catch (error) {
-      throw new Error(error)
+      setMessage('');
+    } finally {
+      setIsLoading(false);
     }
 
   }
@@ -48,18 +48,22 @@ function App() {
         <input type="text" placeholder="Search for a word to get the definition" onChange={e => setSearchWord(e.target.value)} value={searchWord} />
         <button type="submit" onClick={() => HandleApi(searchWord)}>Search</button>
       </main>
-      <section>
-        <h2>{searchWord}</h2>
-        <p>{message}</p>
-
-        {apiLoading ? <p>Loading...</p> : <p>Phonetic: {apiAnswerPho.text}</p>}
-        {apiLoading ? <p>Loading...</p> : <p>Part of Speech: {apiAnswerMea}</p>}
-        {apiLoading ? <p>Loading...</p> : <p>Definition: {apiAnswerDef}</p>}
-        {apiLoading ? <p>Loading...</p> : <p>Part of Speech: {apiAnswerMea2}</p>}
-        {apiLoading ? <p>Loading...</p> : <p>Definition: {apiAnswerDef2}</p>}
+      
+      {isLoading ? (
+          <p>Loading...</p>
+      ) : message ? (
+          <p>{message}</p>
+      ) : ( 
+        <section>
+        {<p>Phonetic: {apiAnswerPho.text}</p>}
+        {<p>Part of Speech: {apiAnswerMea}</p>}
+        {<p>Definition: {apiAnswerDef}</p>}
+        {<p>Part of Speech: {apiAnswerMea2}</p>}
+        {<p>Definition: {apiAnswerDef2}</p>}
         <audio controls test-id='audio' src={apiAnswerAudio}></audio>
       </section>
-
+      )}
+      
     </div>
   )
 }
